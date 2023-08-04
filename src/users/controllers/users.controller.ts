57865ccf134cf112081e08b5acc370d2ab64bfb2
adhes14 <adhemarduran@gmail.com>
2023-08-04 10:46:39 -0400

    add orders by user endpoint

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -12,6 +12,7 @@ import {
   ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
+import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -31,28 +32,33 @@ export class UsersController {
 
   @Get(':userId')
   getUser(@Param('userId', ParseIntPipe) userId: number) {
-    return `this user is ${userId}`;
+    return this.usersService.findOne(userId);
+  }
+
+  @Get(':id/orders')
+  getOrders(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.getOrderByUser(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.ACCEPTED)
-  createUser(@Body() payload: any) {
+  @HttpCode(HttpStatus.CREATED)
+  createUser(@Body() payload: CreateUserDto) {
     return {
       message: 'User created',
-      payload,
+      data: this.usersService.create(payload),
     };
   }
 
   @Put(':userId')
-  update(@Param('userId') userId: string, @Body() payload: any) {
-    return {
-      userId,
-      payload,
-    };
+  update(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Body() payload: UpdateUserDto,
+  ) {
+    return this.usersService.update(userId, payload);
   }
 
   @Delete(':userId')
-  delete(@Param('userId') userId: string) {
-    return userId;
+  delete(@Param('userId', ParseIntPipe) userId: number) {
+    return this.usersService.remove(userId);
   }
 }
